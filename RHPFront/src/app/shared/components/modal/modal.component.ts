e3a32edf5ff978eb8dfbdd3788f844925404e06d
Hleyf <hleyf@hotmail.com>
2024-05-10 16:37:16 +0100

    chore: Update modal component styles and behavior

--- RHPFront/src/app/shared/components/modal/modal.component.ts
+++ RHPFront/src/app/shared/components/modal/modal.component.ts
@@ -1,49 +1,90 @@
-import { Component, ComponentRef, EventEmitter, OnDestroy, Output, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, EventEmitter, Injector, Output, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { IModalOptions } from '../../../services/modal.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
   styleUrls: ['./modal.component.css'],
+  imports: [CommonModule],
   template: `
-    <div class="modal" [class.show]="show" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-body">
-            <ng-container #modalContainer></ng-container>
+    <div class="modal" [class.show]="show" tabindex="-1" (click)="destroy()">
+      <div class="modal-overlay">
+      <div class="modal-dialog" (click)="$event.stopPropagation()">          
+        <div class="modal-content" [style.width]="width" [style.height]="height">
+          @if (header) {
+            <header class="modal-header">
+              <h3 class="modal-title">{{title}}</h3>
+              <button type="button" class="btn-close" (click)="destroy()"></button>
+            </header>
+          }
+            <section class="modal-body">
+              <ng-container #modalContainer></ng-container>
+            </section>
           </div>
         </div>
       </div>
     </div>
   `,
 })
-export class ModalComponent implements OnDestroy {
+export class ModalComponent {
   @ViewChild('modalContainer', { read: ViewContainerRef, static: true }) viewContainerRef!: ViewContainerRef;
   @Output() onClose = new EventEmitter<any>();
-  private componentRef!: ComponentRef<any>;
-  show: boolean = true;
+  @Output() onDestroy = new EventEmitter<any>();
 
-  constructor() {}
-  
-  ngOnDestroy(): void {
-    if (this.componentRef) {
-      this.componentRef.destroy();
-    }
+  show: boolean = true;
+  width: string = 'auto';
+  height: string = 'auto';
+  title: string = '';
+  header: boolean = false;
+
+  ngOnDestroy() {
+    this.viewContainerRef.clear();
   }
 
-  open(component: Type<any>, data?: any) {
-    this.componentRef = this.viewContainerRef.createComponent(component);
-    this.componentRef.instance.data = data;
+  open(component: Type<any>, injector: Injector, options?: IModalOptions) {
+    this.viewContainerRef.clear();
+
+    this.setOptions(options);
+
+    this.viewContainerRef.createComponent(component, {injector})
     this.show = true;	
   }
-
+  
   close(data?: any) {
-    if (this.componentRef) {
-      this.onClose.emit(data);
-      this.componentRef.destroy();
-    }
+    this.onClose.emit(data);
+    this.viewContainerRef.clear();
     this.show = false;
   }
+  
+  destroy() {
+    this.viewContainerRef.clear();
+    this.show = false;
+    this.onDestroy.emit();
+  }
+  
+  private setOptions(options: IModalOptions | undefined) {
+    if (!options) {
+      return;
+    }
+  
+    const { width, height, title, header } = options;
+  
+    if (width) {
+      this.width = width;
+    }
+  
+    if (height) {
+      this.height = height;
+    }
 
-
+    if(header) {
+      this.header = header;
+    }
+      if (title) {
+        this.title = title;
+      }
+  }
 }
+
 
