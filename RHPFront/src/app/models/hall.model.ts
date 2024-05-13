cc3e8e43b50d2f421b1bd09044216da95c5c820e
Hleyf <hleyf@hotmail.com>
2024-05-13 14:19:47 +0100

    chore: Update HallListComponent to use Hall class instead of IHall interface

--- RHPFront/src/app/models/hall.model.ts
+++ RHPFront/src/app/models/hall.model.ts
@@ -6,3 +6,17 @@ export interface IHall {
     description: string;
     players: Player[];
 }
+
+export class Hall implements IHall {
+    id: number;
+    title: string;
+    description: string;
+    players: Player[];
+
+    constructor(data: IHall) {
+        this.id = data.id;
+        this.title = data.title;
+        this.description = data.description;
+        this.players = data.players;
+    }
+}
--- RHPFront/src/app/services/hall.service.ts
+++ RHPFront/src/app/services/hall.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
-import { Observable, catchError, firstValueFrom, of } from 'rxjs';
-import { IHall } from '../models/hall.model';
+import { Observable, catchError, filter, firstValueFrom, map, of } from 'rxjs';
+import { Hall, IHall } from '../models/hall.model';
 import { API_URL } from '../shared/constants';
 
 @Injectable({
@@ -12,12 +12,14 @@ export class HallService {
   constructor(private http: HttpClient) { }
 
   halls = signal<IHall[] | []>([]);
-  hallsLoading = signal<boolean>(false);
-  selectedHall = signal<IHall | null>(null);
+  selectedHall = signal<Hall | null >(null);
+  //Loaders for the hall list and hall view
+  isHallsLoading = signal<boolean>(false);
+  isHallLoading = signal<boolean>(false);
 
   getHalls() {
     console.log('getting halls');
-    this.hallsLoading.set(true);
+    this.isHallsLoading.set(true);
     this.http.get<IHall[]>(API_URL + '/hall').pipe(
       catchError((error) => {
         console.error(error);
@@ -26,24 +28,38 @@ export class HallService {
     ).subscribe((data: IHall[]) => {
       console.log('got halls');
       this.halls.set(data);
-      this.hallsLoading.set(false);
+      this.isHallsLoading.set(false);
     });
   }
 
   getHall(id: number) {
     console.log('getting hall');
+    this.isHallLoading.set(true);
     this.http.get<IHall>(API_URL + '/hall/' + id).pipe(
+      map((data: IHall) => {
+        if (data?.id === id) {
+          return new Hall(data);
+        } else {
+          throw new Error('Hall not found');
+        }
+      }),
       catchError((error) => {
         console.error(error);
-        return of(null)
+        throw error;
       })
-    ).subscribe((data : IHall | null) => {
-      console.log('got hall');
-      this.selectedHall.set(data);
-      console.log('hall: ',data);
+    ).subscribe({
+      next: (hall: Hall) => {
+        this.selectedHall.set(hall);
+        this.isHallLoading.set(false);
+      },
+      error: (error) => {
+        console.error('Error fetching hall:', error);
+        this.isHallLoading.set(false);
+        this.selectedHall.set(null);
+      }
     });
   }
-
+  
   async createHall(hall: IHall): Promise<number> {
     let status: number = 0;
     const response$ = this.http.post<IHall>(API_URL + '/hall', hall, {observe: 'response'});
