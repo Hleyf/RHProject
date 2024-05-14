b59a9e9822b81340b37f00d16f9e8085788ab3dc
Hleyf <hleyf@hotmail.com>
2024-05-14 08:50:45 +0100

    chore: Add search input component

--- RHPFront/src/app/shared/components/search-input/search-input.component.ts
+++ RHPFront/src/app/shared/components/search-input/search-input.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-sesarch-input',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,FontAwesomeModule],
   template: `
     <div>
       <label class="search-label">{{ label }}</label>
@@ -17,22 +20,21 @@ import { FormsModule } from '@angular/forms';
         (keyup.enter)="onSearch()"
       />
       @if (searchTerm) {
-      <button *ngIf="searchTerm" class="clear-icon" (click)="clearSearch()">
-        X
-      </button>
-
+      <fa-icon class="clear-icon" [icon]="closeIcon" (click)="clearSearch()"></fa-icon>
       }
-      <button (click)="onSearch()">Search</button>
+      <!-- <button (click)="onSearch()">Search</button>  -->
+      <!-- TODO: removable tag for reseting searches -->
     </div>
   `,
-  styleUrl: './sesarch-input.component.css',
+  styleUrl: './search-input.component.css',
 })
-export class SesarchInputComponent {
+export class SearchInputComponent {
   @Input() searchFn!: (searchTerm: string) => void;
   @Input() label!: string;
   @Input() placeholder!: string;
 
   searchTerm: string = '';
+  closeIcon = faClose;
 
   constructor() {}
 
